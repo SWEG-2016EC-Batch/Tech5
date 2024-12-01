f0d5a2c9193640d48ad3e25b270c4f0b93af869b
MERON AMARE MENGISTU <meronarahel@gmail.com>
2024-12-01 14:39:39 +0300

    Add files via upload

--- ETS0945 Meron Amare/Power/pow.cpp
+++ ETS0945 Meron Amare/Power/pow.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+int main() {
+
+    double x, y, result;
+
+    cout << "Enter the value of x: ";
+    cin >> x;
+    if(cin.fail()) {
+        cout<<"invalid input"<<endl;
+        return 0;
+    }
+
+    cout << "Enter the value of y: ";
+    cin >> y;
+    if (cin.fail()) {
+        cout<<"invalid input"<<endl;
+        return 0;
+    }
+
+    result = pow(x, y);
+
+
+    cout << "The result of " << x << " raised to the power of " << y << " is: " << result << endl;
+
+    return 0;
+}
