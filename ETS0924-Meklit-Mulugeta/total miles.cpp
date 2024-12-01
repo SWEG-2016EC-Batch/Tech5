3fef4b2298a8ce29e188986fe12a13667907ceee
MEKLIT MULUGETA TAFFA <meklitmulugeta71@gmail.com>
2024-12-01 18:30:32 +0300

    Create total miles.cpp

---
+++ ETS0924-Meklit-Mulugeta/total miles.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float capacity, milesPerGallon, totalMiles;
+
+  // Prompt user for input
+  cout << "Enter the capacity of the fuel tank in gallons: ";
+  cin >> capacity;
+  if (cin.fail() || capacity<0)
+  {
+    cerr << "invalid input for capacity" << endl;
+    return 1;
+  }
+  cout << "Enter the miles per gallon the automobile can drive: ";
+  cin >> milesPerGallon;
+  if (cin.fail()|| milesPerGallon)
+  {
+    cerr << "invalid input for miles per gallon" << endl;
+    return 1;
+  }
+
+  // Calculate total miles
+  totalMiles = capacity * milesPerGallon;
+
+  // Output the result
+  cout << "The automobile can be driven for " << totalMiles << " miles without refueling." << endl;
+
+  return 0;
+}
