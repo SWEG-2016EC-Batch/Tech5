9ac0305be81bb8a9aa5d45e1e7ff0a6b15ff7a87
MELAT FEKADU EJETA <fekadumelat461@gmail.com>
2024-12-02 19:55:47 +0300

    Update salary.cpp

--- ETS0932-Melat-Fekadu/salary.cpp
+++ ETS0932-Melat-Fekadu/salary.cpp
@@ -1,1 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+string employeeName;
+float weeklyhours, bonusrate, basesalary,pension,tax;
+float grossSalary, bonusPayment, netSalary;
+const float pension_rate =0.05;
+const float tax_rate=0.15;
+cout << "please enter your name"<< endl;
+cin >> employeeName;
+cout << "enter your weekly working hours"<< endl;
+cin >> weeklyhours;
+cout << "enter your base salary"<<endl;
+cin >> basesalary;
+cout << "enter your bonus rate"<< endl;
+cin >> bonusrate;
+bonusPayment=bonusrate*weeklyhours;
+
+grossSalary=basesalary+bonusPayment;
+
+pension=grossSalary*pension_rate;
+
+tax=grossSalary*tax_rate;
+
+netSalary=grossSalary-tax-pension;
+
+cout<< "Employee name: "<< employeeName<< endl;
+cout << "bonus payment: "<< bonusPayment<< endl;
+cout<< "gross salary: "<< grossSalary<<endl;
+cout << "your net salary is: "<< netSalary<<endl;
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+
+}
 
