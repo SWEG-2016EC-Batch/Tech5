1e9e1e6953e093b051b7833f74f456cb0827cdd3
MELAT FEKADU EJETA <fekadumelat461@gmail.com>
2024-12-02 19:57:22 +0300

    Update transmission.cpp

--- ETS0932-Melat-Fekadu/transmission.cpp
+++ ETS0932-Melat-Fekadu/transmission.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+int main (){
+double filesizeBytes,transmissionTime ;
+const double transmissionRate=960;
+
+cout << "please enter the file size in Bytes "<< endl;
+cin >> filesizeBytes;
+
+transmissionTime=filesizeBytes/transmissionRate;
+
+cout <<"The transmission time in seconds is: "<<transmissionTime ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
 
