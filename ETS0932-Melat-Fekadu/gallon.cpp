4a4015e62b05671244c74b0b385eb00e08efcc5d
MELAT FEKADU EJETA <fekadumelat461@gmail.com>
2024-12-02 19:53:42 +0300

    Update gallon.cpp

--- ETS0932-Melat-Fekadu/gallon.cpp
+++ ETS0932-Melat-Fekadu/gallon.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+    int milesPerGallon, tankCapacity, mileDriven ;
+
+    cout << "Please enter the capacity of the tank"<< endl;
+    cin >> tankCapacity;
+
+    cout << "Please enter the miles per gallons"<< endl;
+    cin >> milesPerGallon;
+
+    mileDriven = tankCapacity*milesPerGallon;
+
+    cout << " you can drive " << mileDriven<< " miles without refueling.";
+
+    return 0;
+}
