3ef6fc555457c651a761fb2daf667905310edb3a
Nebiyat Eliyas <nebiyate5@gmail.com>
2024-12-02 23:38:33 +0300

    Update Net salary.cpp

--- ETS1112-Nebiyat-Eliyas/Net salary.cpp
+++ ETS1112-Nebiyat-Eliyas/Net salary.cpp
@@ -1,62 +1,45 @@
 #include <iostream>
+#include <string>
 using namespace std;
+int main(){
+string employeeName;
+float weeklyhours, bonusrate, basesalary,pension,tax;
+float grossSalary, bonusPayment, netSalary;
+const float pension_rate =0.05;
+const float tax_rate=0.15;
+cout << "please enter your name"<< endl;
+cin >> employeeName;
+cout << "enter your weekly working hours"<< endl;
+cin >> weeklyhours;
+cout << "enter your base salary"<<endl;
+cin >> basesalary;
+cout << "enter your bonus rate"<< endl;
+cin >> bonusrate;
+bonusPayment=bonusrate*weeklyhours;
 
-int main (){
+grossSalary=basesalary+bonusPayment;
 
-//declare the variable 
-string name;
-double weekly_working_hours, bonus_rate_per_hour,base_salary;
-double gross_salary, net_salary, bonus_payment, pension,tax; 
+pension=grossSalary*pension_rate;
 
-//Ask the user to input that use to calculate nate salary 
+tax=grossSalary*tax_rate;
 
-cout<<"Enter the name of employee: "<< endl;
-cin>>name;
+netSalary=grossSalary-tax-pension;
 
-cout<<"Enter base salary: "<<endl;
-cin>>base_salary;
+cout<< "Employee name: "<< employeeName<< endl;
+cout << "bonus payment: "<< bonusPayment<< endl;
+cout<< "gross salary: "<< grossSalary<<endl;
+cout << "your net salary is: "<< netSalary<<endl;
 
-while(cin.fail()){
-        cin.clear();
-        cin.ignore();
-   cout<<"enter the base salary properly again: ";
-   cin>>base_salary;    
-  }
 
-cout << "Enter weekly working hours: "<<endl;
-cin>>weekly_working_hours;
 
-while(cin.fail()){
-        cin.clear();
-        cin.ignore();
-   cout<<"enter the weekly working hours properly again: ";
-   cin>>weekly_working_hours;    
-  }
 
-cout<<"Enter bonus rate per hour: "<< endl;
-cin>>bonus_rate_per_hour;
 
-while(cin.fail()){
-        cin.clear();
-        cin.ignore();
-   cout<<"enter the bonus rate per hour properly again: ";
-   cin>>bonus_rate_per_hour;    
-  }
-//calculate net salary
-bonus_payment=weekly_working_hours*bonus_rate_per_hour;
-gross_salary=base_salary+bonus_payment;
-tax=gross_salary*0.15;
-pension=base_salary*0.5;
 
-net_salary=gross_salary-tax-pension;
-cout<<"net salary="<<net_salary<<endl;
+
+
+
 
 return 0;
-
-
-
-
-
 
 
 }
