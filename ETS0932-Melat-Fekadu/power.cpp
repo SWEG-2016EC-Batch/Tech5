797e18d71aa811ad3f246f2217901bc180517ec2
MELAT FEKADU EJETA <fekadumelat461@gmail.com>
2024-12-02 19:54:37 +0300

    Update power.cpp

--- ETS0932-Melat-Fekadu/power.cpp
+++ ETS0932-Melat-Fekadu/power.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main (){
+  double x ,y,result;
+  cout << "please enter the value of x" << endl;
+  cin>> x;
+  cout << "please enter the value of y" << endl;
+  cin>> y;
+  result= pow(x,y);
+
+  cout << "The result of "<< x<<"^"<< y<<" is "<< result;
+
+return 0;
+}
 
