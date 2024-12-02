e18e11146c03993950ab14ed54a83cb3916508df
Nebiyat Eliyas <nebiyate5@gmail.com>
2024-12-02 23:40:50 +0300

    Update Power calculator.cpp

--- ETS1112-Nebiyat-Eliyas/Power calculator.cpp
+++ ETS1112-Nebiyat-Eliyas/Power calculator.cpp
@@ -1,38 +1,15 @@
 #include <iostream>
-#include <cmath>  // For the pow function
+#include <cmath>
 using namespace std;
+int main (){
+  double x ,y,result;
+  cout << "please enter the value of x" << endl;
+  cin>> x;
+  cout << "please enter the value of y" << endl;
+  cin>> y;
+  result= pow(x,y);
 
-int main() {
-    // Declare variables
-    double x, y, result;
+  cout << "The result of "<< x<<"^"<< y<<" is "<< result;
 
-    // Input values from the user
-    cout << "Enter the value of x: ";
-    cin >> x;
-    while (cin.fail()){
-    cin.clear();
-    cin.ignore();
-    
-    cout<<"please enter the value of x properly: ";
-    cin>>x;
-    }
-
-    cout << "Enter the value of y: ";
-    cin >> y;
-    
-    while(cin.fail()){
-    cin.clear();
-    cin.ignore();
-    
-    cout<<"please enter the value of y properly: ";
-    cin>>y;
-    }
-
-    // Calculate x^y using the pow function from the cmath library
-    result = pow(x, y);
-
-    // Display the result
-    cout << "Result of x the power of y is: " << result << endl;
-
-    return 0;
+return 0;
 }
