3561bee37a0b5b26a06f19dd127a6a950595f906
Nebiyat Eliyas <nebiyate5@gmail.com>
2024-12-02 23:36:57 +0300

    Update BMI calculator.cpp

--- ETS1112-Nebiyat-Eliyas/BMI calculator.cpp
+++ ETS1112-Nebiyat-Eliyas/BMI calculator.cpp
@@ -1,77 +1,42 @@
 #include <iostream>
 using namespace std;
+int main(){
+int x;
+float w;
+float h;
+float BMI;
+do {
 
-int main()
-{
-  // Declare required variables to store the weight, height, bmi and gender
-  double weight, height, bodyMassIndex;
-  char gender;
-  int continueInpu;
-  // Use a while loop to continue until the user decides to stop
-  while (1 > 0)
-  {
-    // Ask the user to enter weight, height, and gender respectively
-    cout << "Enter weight in kilograms: ";
-    cin >> weight;
-    cout << "Enter height in meters: ";
-    cin >> height;
-    cout << "Enter gender (M/F): ";
-    cin >> gender;
+cout << " please enter your weight in kilogram"<< endl;
+cin >> w;
+cout << "please enter your height in meter"<< endl;
+cin >> h;
+BMI= w/(h*h);
+if(cin.fail() || w<0 && h<0 ){
+    cout << "please enter valid weight and height";
+}
 
-    // Calculate the BMI and display it
-    bodyMassIndex = weight / (height * height);
-    cout << "Your BMI is " << bodyMassIndex << endl;
-    cout << "Your gender is " << gender << endl;
+cout << "your BMI = "<< BMI << endl;
+if (BMI<18.5){
+cout << "under weight"<<endl;
+cout << "you should eat food with high calories"<<endl; }
 
-    // Determine weight level based on BMI and gender
-    // Using if else if stmts
+else if (BMI<=24.9){
+         cout << "normal weight"<< endl;
+         cout <<"keep it up"<<endl;
+         }
+else if(BMI<30){
+    cout << "you are overweight " << endl;
+    cout << "you should balance your diet"<< endl; }
+ else if (BMI>=30){
+    cout << "you are obese"<<endl;
+    cout << "you should eat healthy and drink more water"<<endl;
+ }
+ cout << "enter 0 to stop or enter any number to continue"<<endl;;
+ cin>>x;
+}
+while (x!=0);
 
-    if (cin.fail() || height <= 0 || weight <= 0)
-    {
-      cerr << "Invalid input " << endl;
-      return 1;
-    }
-    else
-    {
-      if (bodyMassIndex >= 30)
-      {
-        cout << "You are obese. It's recommended to consult with a Doctor." << endl;
-      }
-      else if ((gender == 'M' || gender == 'm') && bodyMassIndex < 20.5)
-      {
-        cout << "You are underweight. You must eat balance diet." << endl;
-      }
-      else if ((gender == 'M' || gender == 'm') && bodyMassIndex >= 20.5 && bodyMassIndex < 26)
-      {
-        cout << "You have a normal weight. Keep it up!" << endl;
-      }
-      else if ((gender == 'M' || gender == 'm') && bodyMassIndex >= 26 && bodyMassIndex < 30)
-      {
-        cout << "You are overweight. You must do exercise." << endl;
-      }
-      else if ((gender == 'F' || gender == 'f') && bodyMassIndex < 18.5)
-      {
-        cout << "You are underweight. You must eat balance diet." << endl;
-      }
-      else if ((gender == 'F' || gender == 'f') && bodyMassIndex >= 18.5 && bodyMassIndex < 24)
-      {
-        cout << "You have a normal weight. Keep it up!" << endl;
-      }
-      else if ((gender == 'F' || gender == 'f') && bodyMassIndex >= 24 && bodyMassIndex < 30)
-      {
-        cout << "You are overweight. You must do exercise." << endl;
-      }
-      else
-      {
-        cout << "Invalid gender" << endl;
-      }
-    } // Ask if the user wants to continue
-    cout << "Do you want to continue enter non zero number" << endl;
-    cin >> continueInpu;
-    if (continueInpu == 0)
-    {
-      break;
-    }
-  }
-  return 0;
+
+return 0;
 }
