f7cd8e13956a329cebad1e883c475573e7417061
Meron Alemayehu <183545191+Meri508@users.noreply.github.com>
2024-12-13 09:11:21 +0300

    Create Full pyramid.cpp

---
+++ ETS0943-Meron-Alemayehu/Patterns/Full pyramid.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i = 0; i < 5; i++){
+        for(int k = 5 ; k > i; k--){
+                cout << " ";
+        }
+        for(int j = 0; j < (i * 2)+1; j++){
+            cout << "*";
+        }
+        cout << '\n';     
+    }
+
+    return 0;
+}
