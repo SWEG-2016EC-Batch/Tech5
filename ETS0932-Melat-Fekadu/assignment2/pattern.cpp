1103656b2785e33ceb51067d5872fd8fb9894ff1
MELAT FEKADU EJETA <fekadumelat461@gmail.com>
2024-12-13 20:16:53 +0300

    Update pattern.cpp

--- ETS0932-Melat-Fekadu/assignment2/pattern.cpp
+++ ETS0932-Melat-Fekadu/assignment2/pattern.cpp
@@ -1,1 +1,41 @@
 
+Input
+Number of rows (rows)
+
+Number of columns (columns)
+
+Choice of pattern (choice)
+
+Output
+Different patterns based on the user’s choice:
+
+Rectangle of stars
+
+Square of stars
+
+Half pyramid of stars
+
+Half pyramid of numbers
+
+Inverted half pyramid of stars
+
+Half pyramid of alphabets
+
+Inverted half pyramid of alphabets
+
+Full pyramid of stars
+
+Inverted full pyramid of stars
+
+Hollow full pyramid of stars
+
+Inverted hollow full pyramid of stars
+
+Process
+Prompt the user to enter the number of rows.
+
+Prompt the user to enter the number of columns.
+
+Prompt the user to enter their choice of pattern.
+
+Based on the user’s choice, generate and display the corresponding pattern.
