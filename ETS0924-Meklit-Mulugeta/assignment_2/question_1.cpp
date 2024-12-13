6d6fa2eae3380bfea3a24d550b8cf4ca8a1a907e
MEKLIT MULUGETA TAFFA <meklitmulugeta71@gmail.com>
2024-12-13 02:57:16 -0800

    Update question_1.cpp

--- ETS0924-Meklit-Mulugeta/assignment_2/question_1.cpp
+++ ETS0924-Meklit-Mulugeta/assignment_2/question_1.cpp
@@ -1,1 +1,45 @@
+#include <iostream> 
+using namespace std; 
+int main(){ 
+double weeklyHours, bonusRate, baseSalary,pension,tax=0.0; 
+double grossSalary, bonusPayment=0.0, netSalary; 
+const double pension_rate =0.07; 
+cout << "enter your weekly working hours"<< endl; 
+cin >> weeklyHours; 
+cout << "enter your base salary"<<endl; 
+cin >> baseSalary; 
+if (weeklyHours > 40) {
+          cout << "Enter bonus rate per hour: ";
+          cin >> bonusRate;
+          // Calculate gross salary and bonus payment
+          bonusPayment = weeklyHours * bonusRate;
+    }
+  
+ 
+grossSalary=baseSalary+bonusPayment; 
+ 
+pension=baseSalary*pension_rate; 
+ 
+if(grossSalary>=200 && grossSalary<=600){
+    tax=grossSalary*0.1;
+}else if(grossSalary>600 && grossSalary<=1200){
+    tax=grossSalary*0.15;
+}else if(grossSalary>1200 && grossSalary<=2000){
+    tax=grossSalary*0.2;
+}else if (grossSalary>2000 && grossSalary<=3500){
+    tax=grossSalary*0.25;
+}else if (grossSalary>3500 ){
+    tax=grossSalary*0.3;
+}
 
+netSalary=grossSalary-tax-pension; 
+ 
+cout << "bonus payment: "<< bonusPayment<< endl; 
+cout<< "gross salary: "<< grossSalary<<endl; 
+cout << "your net salary is: "<< netSalary<<endl; 
+ 
+ 
+return 0; 
+ 
+ 
+}
